5905e0381a89c728189b55cedebaf32dd014bc31
hye <eunhyekim811@gmail.com>
2024-09-17 16:20:03 +0900

    initial commit

---
+++ src/main/java/com/example/blackBean/BlackBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackBeanApplication.class, args);
+	}
+
+}
