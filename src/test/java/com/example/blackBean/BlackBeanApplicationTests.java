70117e79142ad9869ebdbaac35dcbbe8ada9690f
hye <eunhyekim811@gmail.com>
2024-09-22 16:50:31 +0900

    v2(java 17) first commit

---
+++ src/test/java/com/example/blackBean/BlackBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
